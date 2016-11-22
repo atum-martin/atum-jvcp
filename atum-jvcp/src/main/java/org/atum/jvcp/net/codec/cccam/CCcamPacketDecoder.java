d19258896fa0f1193d536804cbe3070dabc3352f
atum-martin <martin.chapman4@mail.dcu.ie>
2016-11-22 22:28:02 +0000

    Added packet command ids.

--- atum-jvcp/src/main/java/org/atum/jvcp/net/codec/cccam/CCcamPacketDecoder.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/cccam/CCcamPacketDecoder.java
@@ -14,6 +14,29 @@ public class CCcamPacketDecoder extends ByteToMessageDecoder {
 
 	private Logger logger = Logger.getLogger(CCcamPacketDecoder.class);
 	
+	//Taken from Oscam CCcam implementation.
+	@SuppressWarnings("unused")
+	private static final int MSG_CLI_DATA = 0,
+			MSG_CW_ECM = 1,
+			MSG_EMM_ACK = 2,
+			MSG_CARD_REMOVED = 4,
+			MSG_CMD_05 = 5,
+			MSG_KEEPALIVE = 6,
+			MSG_NEW_CARD = 7,
+			MSG_SRV_DATA = 8,
+			MSG_CMD_0A = 0x0a,
+			MSG_CMD_0B = 0x0b,
+			MSG_CMD_0C = 0x0c, // CCCam 2.2.x fake client checks
+			MSG_CMD_0D = 0x0d, // "
+			MSG_CMD_0E = 0x0e, // "
+			MSG_NEW_CARD_SIDINFO = 0x0f,
+			MSG_SLEEPSEND = 0x80, //Sleepsend support
+			MSG_CACHE_PUSH = 0x81, //CacheEx Cache-Push In/Out
+			MSG_CACHE_FILTER = 0x82, //CacheEx Cache-Filter Request
+			MSG_CW_NOK1 = 0xfe, //Node no more available
+			MSG_CW_NOK2 = 0xff, //No decoding
+			MSG_NO_HEADER = 0xffff;
+	
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 		//cc_crypt(&cc->block[DECRYPT], buf, 4, DECRYPT);
@@ -31,6 +54,19 @@ public class CCcamPacketDecoder extends ByteToMessageDecoder {
 		NetUtils.readBuffer(in, payload, payload.length);
 		session.getDecrypter().decrypt(payload, payload.length);
 		
+		handlePacket(session, cmdCode, payload);
+		
+	}
+
+	private void handlePacket(CCcamSession session, int cmdCode, byte[] payload) {
+		switch (cmdCode) {
+		case MSG_CLI_DATA:
+			
+			break;
+		default:
+			logger.info("unhandled packet: "+cmdCode+" "+payload.length);
+			break;
+		}
 	}
 
 
