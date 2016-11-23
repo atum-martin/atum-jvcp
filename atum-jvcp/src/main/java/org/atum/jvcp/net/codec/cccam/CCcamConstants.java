16a51bebc8d608cab02db215a4ee56632ffc2857
atum-martin <martin.chapman4@mail.dcu.ie>
2016-11-23 00:08:03 +0000

    Added CCcam constants class.

---
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/cccam/CCcamConstants.java
@@ -0,0 +1,27 @@
+package org.atum.jvcp.net.codec.cccam;
+
+public class CCcamConstants {
+
+	//Taken from Oscam CCcam implementation.
+	@SuppressWarnings("unused")
+	public static final int MSG_CLI_DATA = 0,
+			MSG_CW_ECM = 1,
+			MSG_EMM_ACK = 2,
+			MSG_CARD_REMOVED = 4,
+			MSG_CMD_05 = 5,
+			MSG_KEEPALIVE = 6,
+			MSG_NEW_CARD = 7,
+			MSG_SRV_DATA = 8,
+			MSG_CMD_0A = 0x0a,
+			MSG_CMD_0B = 0x0b,
+			MSG_CMD_0C = 0x0c, // CCCam 2.2.x fake client checks
+			MSG_CMD_0D = 0x0d, // "
+			MSG_CMD_0E = 0x0e, // "
+			MSG_NEW_CARD_SIDINFO = 0x0f,
+			MSG_SLEEPSEND = 0x80, //Sleepsend support
+			MSG_CACHE_PUSH = 0x81, //CacheEx Cache-Push In/Out
+			MSG_CACHE_FILTER = 0x82, //CacheEx Cache-Filter Request
+			MSG_CW_NOK1 = 0xfe, //Node no more available
+			MSG_CW_NOK2 = 0xff, //No decoding
+			MSG_NO_HEADER = 0xffff;
+}
--- atum-jvcp/src/main/java/org/atum/jvcp/net/codec/cccam/CCcamPacketDecoder.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/cccam/CCcamPacketDecoder.java
@@ -15,29 +15,6 @@ public class CCcamPacketDecoder extends ByteToMessageDecoder {
 
 	private Logger logger = Logger.getLogger(CCcamPacketDecoder.class);
 	
-	//Taken from Oscam CCcam implementation.
-	@SuppressWarnings("unused")
-	private static final int MSG_CLI_DATA = 0,
-			MSG_CW_ECM = 1,
-			MSG_EMM_ACK = 2,
-			MSG_CARD_REMOVED = 4,
-			MSG_CMD_05 = 5,
-			MSG_KEEPALIVE = 6,
-			MSG_NEW_CARD = 7,
-			MSG_SRV_DATA = 8,
-			MSG_CMD_0A = 0x0a,
-			MSG_CMD_0B = 0x0b,
-			MSG_CMD_0C = 0x0c, // CCCam 2.2.x fake client checks
-			MSG_CMD_0D = 0x0d, // "
-			MSG_CMD_0E = 0x0e, // "
-			MSG_NEW_CARD_SIDINFO = 0x0f,
-			MSG_SLEEPSEND = 0x80, //Sleepsend support
-			MSG_CACHE_PUSH = 0x81, //CacheEx Cache-Push In/Out
-			MSG_CACHE_FILTER = 0x82, //CacheEx Cache-Filter Request
-			MSG_CW_NOK1 = 0xfe, //Node no more available
-			MSG_CW_NOK2 = 0xff, //No decoding
-			MSG_NO_HEADER = 0xffff;
-	
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
 		//cc_crypt(&cc->block[DECRYPT], buf, 4, DECRYPT);
@@ -61,7 +38,7 @@ public class CCcamPacketDecoder extends ByteToMessageDecoder {
 
 	private void handlePacket(CCcamSession session, int cmdCode, ByteBuf payload) {
 		switch (cmdCode) {
-		case MSG_CLI_DATA:
+		case CCcamConstants.MSG_CLI_DATA:
 			String username = NetUtils.readCCcamString(payload,20);
 			
 			long nodeId = payload.readLong();
@@ -71,7 +48,7 @@ public class CCcamPacketDecoder extends ByteToMessageDecoder {
 			String build = NetUtils.readCCcamString(payload,32);
 			logger.info("MSG_CLI_DATA: "+version+" "+build+" "+username+" "+nodeId);
 			break;
-		case MSG_SRV_DATA:
+		case CCcamConstants.MSG_SRV_DATA:
 			nodeId = payload.readLong();
 			version = NetUtils.readCCcamString(payload,32);
 			build = NetUtils.readCCcamString(payload,32);
