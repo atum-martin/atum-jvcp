8466d6480fc09b70a5f2430789e007648a46cab2
Martin <martin.chapman4@mail.dcu.ie>
2016-11-26 20:43:08 +0000

    added cccam server thread to keep cccam sessions alive.

--- atum-jvcp/src/main/java/org/atum/jvcp/CCcamServer.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/CCcamServer.java
@@ -1,9 +1,12 @@
 package org.atum.jvcp;
 
+import java.util.ArrayList;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.atum.jvcp.account.AccountStore;
 import org.atum.jvcp.net.NettyBootstrap;
+import org.atum.jvcp.net.codec.cccam.CCcamSession;
 
 /**
  * A basic socket acceptor which will decode the CCcam handshake and print the username sent.
@@ -13,20 +16,50 @@ import org.atum.jvcp.net.NettyBootstrap;
  *
  */
 
-public class CCcamServer {
+public class CCcamServer extends Thread {
 
 	@SuppressWarnings("unused")
 	private Logger logger = Logger.getLogger(CCcamServer.class);
+	private ArrayList<CCcamSession> sessionList = new ArrayList<CCcamSession>();
+	
 	
 	public CCcamServer(int port) {
 		BasicConfigurator.configure();
 		AccountStore.getSingleton();
 		
-		NettyBootstrap.listen(port);
+		NettyBootstrap.listen(this,port);
+		this.start();
+	}
+	
+	public void run(){
+		while(true){
+			synchronized(sessionList){
+				sendKeepAlives();
+			}
+			try {
+				Thread.sleep(200L);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void sendKeepAlives() {
+		for(CCcamSession session : sessionList){
+			if(session.getLastKeepalive() > 30000){
+				session.keepAlive();
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		new CCcamServer(12000);
+	}
+
+	public void registerSession(CCcamSession session) {
+		synchronized (sessionList){
+			sessionList.add(session);
+		}
 	}
 
 }
