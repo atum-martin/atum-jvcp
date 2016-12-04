2f078759a6a1a231876357bb43811fbe524ba4ed
atum-martin <martin.chapman4@mail.dcu.ie>
2016-12-04 19:05:10 +0000

    Added SHA decoding to cccam client.

--- atum-jvcp/src/main/java/org/atum/jvcp/net/codec/cccam/io/CCcamClientLoginDecoder.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/cccam/io/CCcamClientLoginDecoder.java
@@ -2,25 +2,76 @@ package org.atum.jvcp.net.codec.cccam.io;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.atum.jvcp.CCcamServer;
 import org.atum.jvcp.net.LoginDecoder;
+import org.atum.jvcp.net.NetworkConstants;
+import org.atum.jvcp.net.codec.LoginState;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 
 public class CCcamClientLoginDecoder extends LoginDecoder {
 
-	public CCcamClientLoginDecoder(CCcamServer server) {
+	private Logger logger = Logger.getLogger(CCcamServerLoginDecoder.class);
+	
+	public CCcamClientLoginDecoder(CCcamServer server, String username, String password) {
 		super(server);
 	}
 
 	@Override
 	public void init(ChannelHandlerContext firstContext) {
-
+		firstContext.channel().attr(NetworkConstants.LOGIN_STATE).set(LoginState.HANDSHAKE);
 	}
 
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+		LoginState state = ctx.channel().attr(NetworkConstants.LOGIN_STATE).get();
+		logger.info("processing state: " + state);
+		switch (state) {
+		case HANDSHAKE:
+			handleHandshake(ctx, in);
+			break;
+		case HEADER:
+			handleLoginHeader(ctx, in);
+			break;
+		case LOGIN_BLOCK_HEADER:
+			handleLoginBlockHeader(ctx, in);
+			break;
+		default:
+			throw new IllegalStateException("Invalid state during login decoding.");
+		}
+	}
+
+	private void handleLoginBlockHeader(ChannelHandlerContext ctx, ByteBuf in) {
+		if (in.readableBytes() < 16) {
+			logger.info("less than 16 bytes in client buffer");
+			return;
+		}
+		byte[] secureRandom = new byte[16];
+		in.readBytes(secureRandom);
+		
+		boolean isOscam = testOscamSha(secureRandom);
+		boolean isMultiCs = false;
+		
+		
+	}
+
+	private boolean testOscamSha(byte[] secureRandom) {
+		int recv_sum = (secureRandom[14] << 8) | secureRandom[15];
+		int sum = 0x1234;
+		for(int i = 0; i < 14; i++)
+		{
+			sum += secureRandom[i] & 0xFF;
+		}
+		return recv_sum == sum;
+	}
+
+	private void handleLoginHeader(ChannelHandlerContext ctx, ByteBuf in) {
+		
+	}
+
+	private void handleHandshake(ChannelHandlerContext ctx, ByteBuf in) {
 		
 	}
 
