b6a69e44c4cf43435765ea136e26f7b1ff1018df
atum-martin <martin.chapman4@mail.dcu.ie>
2016-12-14 23:00:08 +0000

    Added some additional documentation for accounts.

--- atum-jvcp/src/main/java/org/atum/jvcp/account/Account.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/account/Account.java
@@ -7,27 +7,51 @@ import java.util.ArrayList;
  * @since 21 Nov 2016 22:47:32
  */
 
-
 public class Account {
 
+	/**
+	 * Strings representing the username and password of the account. Due to
+	 * poor CCcam implementation this must be kept in plaintext.
+	 */
 	private String username, password;
+	/**
+	 * A list of integers that represents the groups this user is apart of.
+	 */
 	@SuppressWarnings("unused")
 	private ArrayList<Integer> groups = new ArrayList<Integer>(2);
-	
+
+	/**
+	 * Constructs a new Account object with a specified username and password.
+	 * This is not persisted.
+	 * 
+	 * @param username
+	 * @param password
+	 */
 	public Account(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
-	
-	public String toString(){
+
+	/**
+	 * Returns a string with the properties of this user account.
+	 */
+	public String toString() {
 		return username;
 	}
 
+	/**
+	 * Returns the username associated with this account.
+	 * @return Returns the username associated with this account.
+	 */
 	public String getUsername() {
 		return username;
 	}
-	
-	public String getPassword(){
+
+	/**
+	 * Returns the password associated with this account.
+	 * @return Returns the password associated with this account.
+	 */
+	public String getPassword() {
 		return password;
 	}
 }
--- atum-jvcp/src/main/java/org/atum/jvcp/account/AccountStore.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/account/AccountStore.java
@@ -10,36 +10,69 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 /**
+ * A simple json account store that uses gson to load the accounts.json file.
+ * 
  * @author <a href="https://github.com/atum-martin">atum-martin</a>
  * @since 18 Nov 2016 20:58:29
  */
 
 public class AccountStore {
 
+	/**
+	 * The instance of the log4j {@link Logger}.
+	 */
 	private Logger logger = Logger.getLogger(AccountStore.class);
-	private HashMap<String,Account> accountStore = new HashMap<String,Account>();
+
+	/**
+	 * A map of {link: {@link Account#getUsername()} that maps to the user
+	 * {@link Account} which contains the users settings.
+	 */
+	private HashMap<String, Account> accountStore = new HashMap<String, Account>();
+
+	/**
+	 * Only one instance of the account store should be loaded so a singleton is
+	 * used.
+	 */
 	private static AccountStore singleton = null;
-	
-	public static AccountStore getSingleton(){
-		if( singleton == null){
+
+	/**
+	 * Uses a singleton to obtain an instance of {link: AccountStore}.
+	 * 
+	 * @return the only instance of the {link: AccountStore}.
+	 */
+	public static AccountStore getSingleton() {
+		if (singleton == null) {
 			singleton = new AccountStore();
 		}
 		return singleton;
 	}
 
+	/**
+	 * The default constructor. Will load the resource accounts.json and parse
+	 * using google gson. Any null entries are cleaned from the output before
+	 * being added to the account map {link: {@link AccountStore#accountStore}
+	 */
 	public AccountStore() {
 		Gson gson = new GsonBuilder().create();
 		InputStream is = this.getClass().getClassLoader().getResourceAsStream("accounts.json");
 		Account[] accounts = gson.fromJson(new InputStreamReader(is), Account[].class);
 		for (Account account : accounts) {
-			if (account != null){
+			if (account != null) {
 				logger.info("Creating account: " + account);
-				accountStore.put(account.getUsername(),account);
+				accountStore.put(account.getUsername(), account);
 			}
 		}
 	}
-	
-	public Account getAccount(String username){
+
+	/**
+	 * An {link: Account} object containing the user settings for the specified
+	 * user. Returns null in the event no account is found.
+	 * 
+	 * @param username
+	 * @return An {link: Account} object containing the user settings for the
+	 *         specified user. Returns null in the event no account is found.
+	 */
+	public Account getAccount(String username) {
 		return accountStore.get(username);
 	}
 
