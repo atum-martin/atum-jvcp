833072fefc61abe9e1f32a3c7bebc18fe4d492d0
atum-martin <martin.chapman4@mail.dcu.ie>
2016-12-15 23:51:26 +0000

    Newcamd client login decoder class

---
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/newcamd/NewcamdClientLoginDecoder.java
@@ -0,0 +1,82 @@
+package org.atum.jvcp.net.codec.newcamd;
+
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+
+import org.apache.log4j.Logger;
+import org.atum.jvcp.NewcamdServer;
+import org.atum.jvcp.net.LoginDecoder;
+import org.atum.jvcp.net.NetworkConstants;
+import org.atum.jvcp.net.codec.LoginState;
+
+/**
+ * @author <a href="https://github.com/atum-martin">atum-martin</a>
+ * @since 13 Dec 2016 23:43:11
+ */
+public class NewcamdClientLoginDecoder extends LoginDecoder {
+
+	private Logger logger = Logger.getLogger(NewcamdClientLoginDecoder.class);
+	
+	public NewcamdClientLoginDecoder(NewcamdServer newcamdServer) {
+		super(newcamdServer);
+	}
+
+	@Override
+	protected void decode(ChannelHandlerContext context, ByteBuf buffer, List<Object> outStream) throws Exception {
+		LoginState state = context.channel().attr(NetworkConstants.LOGIN_STATE).get();
+		logger.info("processing state: " + state);
+		switch (state) {
+
+		case SHA:
+			// this shouldn't be getting hit.
+			handleSHA(context);
+			break;
+		case HANDSHAKE:
+			handleHandshake(context, buffer);
+			break;
+		case HEADER:
+			handleLoginHeader(context, buffer);
+			break;
+		case LOGIN_BLOCK_HEADER:
+			handleLoginBlockHeader(context, buffer);
+			break;
+		default:
+			throw new IllegalStateException("Invalid state during login decoding.");
+		}
+	}
+
+	public void init(ChannelHandlerContext context) {
+		
+	}
+
+	private void handleSHA(ChannelHandlerContext context) {
+		
+
+	}
+
+	private void handleHandshake(ChannelHandlerContext context, ByteBuf buffer) {
+
+		if (buffer.readableBytes() < 20) {
+			logger.info("less than 20 bytes in buffer");
+			return;
+		}
+
+	}
+
+	private void handleLoginHeader(ChannelHandlerContext context, ByteBuf buffer) {
+		if (buffer.readableBytes() < 20) {
+			logger.info("less than 20 bytes in buffer");
+			return;
+		}
+
+		context.channel().attr(NetworkConstants.LOGIN_STATE).set(LoginState.LOGIN_BLOCK_HEADER);
+	}
+
+	private void handleLoginBlockHeader(ChannelHandlerContext context, ByteBuf buffer) {
+
+	}
+
+
+}
