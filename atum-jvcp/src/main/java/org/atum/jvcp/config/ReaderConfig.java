2a5534e9a1beaa05ae5d4037328eaa0542429d05
Martin <martin.chapman4@mail.dcu.ie>
2017-01-02 16:00:01 +0000

    Added verification to reader.json parsing.

--- atum-jvcp/src/main/java/org/atum/jvcp/config/ReaderConfig.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/config/ReaderConfig.java
@@ -13,7 +13,9 @@ import org.atum.jvcp.CCcamServer;
 import org.atum.jvcp.CamServer;
 import org.atum.jvcp.NewcamdServer;
 import org.atum.jvcp.account.Account;
+import org.atum.jvcp.net.codec.NetUtils;
 import org.atum.jvcp.net.codec.cccam.CCcamClient;
+import org.atum.jvcp.net.codec.newcamd.NewcamdClient;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -43,48 +45,73 @@ public class ReaderConfig {
 		JvcpReader[] readers = gson.fromJson(new InputStreamReader(is), JvcpReader[].class);
 		for (JvcpReader reader : readers) {
 			if (reader != null) {
+				if (!verifyReaderBasics(reader)) {
+					logger.info("reader failed basic validation: "+reader);
+					continue;
+				}
 				logger.info("loaded reader: " + reader.protocol + "://" + reader.host + ":" + reader.port + " " + reader.username);
+
 				if (reader.protocol.equalsIgnoreCase("cccam")) {
 					CCcamServer server = getCCcamServer(reader.server);
-					if(server == null)
+					if (server == null)
 						continue;
 					CCcamClient.connect(server, reader.host, reader.port, new Account(reader.username, reader.password));
 				} else if (reader.protocol.equalsIgnoreCase("newcamd")) {
 					NewcamdServer server = getNewcamdServer(reader.server);
-					if(server == null)
+					if (server == null)
 						continue;
-					//CCcamClient.connect(server, reader.host, reader.port, new Account(reader.username, reader.password));
+					if (reader.desKey == null) {
+						logger.info("no desKey set for newcamd reader: " + reader.name);
+						continue;
+					}
+					NewcamdClient.connect(server, reader.host, reader.port, new Account(reader.username, reader.password), NetUtils.getBytesValue(reader.desKey));
 				}
 			}
 		}
 	}
 
+	private boolean verifyReaderBasics(JvcpReader reader) {
+		if(reader.host == null)
+			return false;
+		if(reader.name == null)
+			return false;
+		if(reader.protocol == null)
+			return false;
+		if(reader.username == null)
+			return false;
+		if(reader.password == null)
+			return false;
+		if(reader.port == 0)
+			return false;
+		return true;
+	}
+
 	private CCcamServer getCCcamServer(String serverName) {
-		if(cccamServers.size() == 0){
-			logger.info("no server found to bind reader to. Missing server name: "+serverName);
+		if (cccamServers.size() == 0) {
+			logger.info("no server found to bind reader to. Missing server name: " + serverName);
 			return null;
 		}
-		if(serverName == null){
+		if (serverName == null) {
 			return cccamServers.get(0);
 		}
-		for(CCcamServer serv : cccamServers){
-			if(serv.getName().equalsIgnoreCase(serverName)){
+		for (CCcamServer serv : cccamServers) {
+			if (serv.getName().equalsIgnoreCase(serverName)) {
 				return serv;
 			}
 		}
 		return cccamServers.get(0);
 	}
-	
+
 	private NewcamdServer getNewcamdServer(String serverName) {
-		if(newcamdsServers.size() == 0){
-			logger.info("no server found to bind reader to. Missing server name: "+serverName);
+		if (newcamdsServers.size() == 0) {
+			logger.info("no server found to bind reader to. Missing server name: " + serverName);
 			return null;
 		}
-		if(serverName == null){
+		if (serverName == null) {
 			return newcamdsServers.get(0);
 		}
-		for(NewcamdServer serv : newcamdsServers){
-			if(serv.getName().equalsIgnoreCase(serverName)){
+		for (NewcamdServer serv : newcamdsServers) {
+			if (serv.getName().equalsIgnoreCase(serverName)) {
 				return serv;
 			}
 		}
@@ -110,5 +137,8 @@ public class ReaderConfig {
 		private String host;
 		private String server = null;
 		private int port;
+
+		/** protocol specific settings: */
+		private String desKey = null;
 	}
 }
---
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/newcamd/NewcamdClient.java
@@ -0,0 +1,34 @@
+package org.atum.jvcp.net.codec.newcamd;
+
+import org.atum.jvcp.NewcamdServer;
+import org.atum.jvcp.account.Account;
+import org.atum.jvcp.net.ClientConnector;
+import org.atum.jvcp.net.NetworkConstants;
+import org.atum.jvcp.net.codec.newcamd.io.NewcamdClientLoginDecoder;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @author <a href="https://github.com/atum-martin">atum-martin</a>
+ * @since 3 Dec 2016 14:57:43
+ */
+
+public class NewcamdClient extends NewcamdSession {
+
+	public NewcamdClient(ChannelHandlerContext context,NewcamdServer server, byte[] desKey) {
+		super(context, desKey);
+	}
+
+	public static NewcamdClient connect(NewcamdServer server, String host,int port, Account account, byte[] desKey){
+		ClientConnector conn = new ClientConnector();
+		
+		Channel channel = conn.connect(host, port, account, new NewcamdPipeline(server, NewcamdClientLoginDecoder.class));
+		NewcamdClient client = new NewcamdClient(channel.pipeline().firstContext(), server, desKey);
+		channel.attr(NetworkConstants.CAM_SESSION).set(client);
+		client.setReader(true);
+		server.registerSession(client);
+		return client;
+	}
+
+}
