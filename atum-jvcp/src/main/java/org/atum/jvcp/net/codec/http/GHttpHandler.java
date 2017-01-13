cd62cb05be7bdf4d4da2f385e4aa94800dc6e24f
atum-martin <martin.chapman4@mail.dcu.ie>
2017-01-13 23:23:55 +0000

    missing ghttp class file.

---
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/http/GHttpHandler.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package org.atum.jvcp.net.codec.http;
+
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpResponse;
+
+/**
+ * @author <a href="https://github.com/atum-martin">atum-martin</a>
+ * @since 13 Jan 2017 23:18:27
+ */
+public class GHttpHandler {
+	
+	public void handleGetRequest(HttpRequest msg, HttpResponse response){
+		
+	}
+}
