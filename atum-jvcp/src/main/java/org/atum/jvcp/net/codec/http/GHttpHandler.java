27699adc118b1560aa84e937d1386b0f0cf4ac39
atum-martin <martin.chapman4@mail.dcu.ie>
2017-01-14 23:37:05 +0000

    added GHttp API endpoint urls.

--- atum-jvcp/src/main/java/org/atum/jvcp/net/codec/http/GHttpHandler.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/http/GHttpHandler.java
@@ -11,8 +11,15 @@ import io.netty.handler.codec.http.HttpResponse;
  * @since 13 Jan 2017 23:18:27
  */
 public class GHttpHandler {
-	
-	public void handleGetRequest(HttpRequest msg, HttpResponse response){
-		
+
+	protected static final String API_PREFIX = "/api";
+
+	protected static final String API_CACHE_GET = API_PREFIX + "/c/";
+	protected static final String API_ECM_POST = API_PREFIX + "/e/";
+	protected static final String API_FEEDER_POST = API_PREFIX + "/f/";
+	protected static final String API_CAPMT = API_PREFIX + "/p/";
+
+	public void handleGetRequest(HttpRequest msg, HttpResponse response) {
+
 	}
 }
