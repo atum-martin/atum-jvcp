ab97061dbdadc710be54aceb355189a07fa31781
atum-martin <martin.chapman4@mail.dcu.ie>
2017-01-26 23:33:44 +0000

    Added cache wait time before sending ECM to readers.

--- atum-jvcp/src/main/java/org/atum/jvcp/CardServer.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/CardServer.java
@@ -6,6 +6,7 @@ package org.atum.jvcp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map.Entry;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
@@ -34,27 +35,41 @@ public class CardServer {
 	private static ClusteredCache pendingEcms = new ClusteredCache();
 	private static ArrayList<CamSession> readers = new ArrayList<CamSession>();
 	private static int readerRoundRobin = 0;
-	
+
 	private static final boolean DCW_CHECKING = true;
 
 	/**
 	 * Main method used to initialize a blank cccam server with clustered cache.
 	 * 
-	 * @param args Not used
+	 * @param args
+	 *            Not used
 	 */
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 		BasicConfigurator.configure();
 		Logger.getRootLogger().setLevel(Level.INFO);
-		
+
 		ChannelList.getSingleton();
 		AccountStore.getSingleton();
 		CCcamServer server1 = new CCcamServer("cccam-server1", 12000);
 		NewcamdServer server2 = new NewcamdServer("newcamd-server1", 12001);
-		NettyBootstrap.listenTcp(new HttpPipeline(),8080);
-		ReaderConfig config = new ReaderConfig(new CamServer[]{server1, server2,});
+		NettyBootstrap.listenTcp(new HttpPipeline(), 8080);
+		ReaderConfig config = new ReaderConfig(new CamServer[] { server1, server2, });
 		addAllReaders(server1, readers);
 		addAllReaders(server2, readers);
+		
+		while(true){
+			try {
+				fireCacheWaitTimeout();
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+			try {
+				Thread.sleep(5);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
@@ -82,30 +97,32 @@ public class CardServer {
 	}
 
 	public static boolean handleEcmAnswer(CamSession session, int cspHash, byte[] cw, int cardId, int serviceId) {
-		//long ecmHash = EcmRequest.computeEcmHash(cspHash);
-		//logger.info("ECM cache entry for: "+cspHash);
+		// long ecmHash = EcmRequest.computeEcmHash(cspHash);
+		// logger.info("ECM cache entry for: "+cspHash);
 		EcmRequest req = CardServer.getPendingCache().peekCache(cspHash);
 		if (req != null) {
 			req.setDcw(cw);
 			getCache().addEntry(cspHash, req);
 			getPendingCache().removeRequest(cspHash);
-			logger.info("ecm answer for "+ChannelList.getChannelName(req.getCardId(), req.getServiceId())+" by "+session+" for "+req.getSessionsStr()+" sessions.");
+			logger.info("ecm answer for " + ChannelList.getChannelName(req.getCardId(), req.getServiceId()) + " by " + session + " for " + req.getSessionsStr()
+					+ " sessions.");
 
 			req.fireActionListeners();
-			//logger.info("Cache push hit on: "+Integer.toHexString(cardId)+":"+Integer.toHexString(serviceId));
-			//logger.info("cache hit for pending dcw dump: "+NetUtils.bytesToString(cw,0,cw.length));
+			// logger.info("Cache push hit on: "+Integer.toHexString(cardId)+":"+Integer.toHexString(serviceId));
+			// logger.info("cache hit for pending dcw dump: "+NetUtils.bytesToString(cw,0,cw.length));
 			return true;
 		} else {
-			//EcmRequest either exists in cache or no pending requests have come in.
-			//We want to check does the DCW differ from the cache entry.
+			// EcmRequest either exists in cache or no pending requests have
+			// come in.
+			// We want to check does the DCW differ from the cache entry.
 			req = CardServer.getCache().peekCache(cspHash);
 		}
-		
+
 		if (req != null) {
-			//this should never fail
+			// this should never fail
 			if (DCW_CHECKING && req.hasAnswer()) {
 				if (!Arrays.equals(cw, req.getDcw())) {
-					//logger.warn("Duplicate ECM with different DCW. "+cspHash+" "+sum(cw));
+					// logger.warn("Duplicate ECM with different DCW. "+cspHash+" "+sum(cw));
 				}
 			}
 			return true;
@@ -113,22 +130,38 @@ public class CardServer {
 		return false;
 	}
 
-	public static EcmRequest createEcmRequest(CamSession session, int cardId, int provider, int shareId, int serviceId, byte[] ecm, int cspHash, boolean cache){
-		if(cspHash == 0L){
-			//no hash found compute it.
+	public static void fireCacheWaitTimeout() {
+		long now = System.currentTimeMillis();
+		synchronized (pendingEcms) {
+			for (Object o : pendingEcms.entrySet()) {
+				Entry<Long, EcmRequest> e = (Entry) o;
+				EcmRequest req = e.getValue();
+				if (now - req.getTimestamp() < 150) {
+					// map is ordered no entry after this point will have
+					// expired
+					break;
+				}
+				if (!sendEcmToReader(req)) {
+					logger.error("no reader found for ecm req: " + req);
+				}
+			}
+		}
+	}
+
+	public static EcmRequest createEcmRequest(CamSession session, int cardId, int provider, int shareId, int serviceId, byte[] ecm, int cspHash, boolean cache) {
+		if (cspHash == 0L) {
+			// no hash found compute it.
 			cspHash = EcmRequest.computeEcmHash(ecm);
 		}
 		EcmRequest answer = CardServer.getPendingCache().peekCache(cspHash);
-		if(answer != null && !cache){
+		if (answer != null && !cache) {
 			answer.addGroups(session);
 			return answer;
 		}
 		answer = new EcmRequest(session, cardId, provider, shareId, serviceId, ecm, false);
 		answer.setCspHash(cspHash);
-		if(!cache){
-			if(!sendEcmToReader(answer)){
-				logger.error("no reader found for ecm req: "+answer);
-			}		
+		if (!cache) {
+
 			getPendingCache().addEntry(cspHash, answer);
 		}
 		return answer;
@@ -138,10 +171,10 @@ public class CardServer {
 	 * @param answer
 	 */
 	private static boolean sendEcmToReader(EcmRequest req) {
-		if(readers.size() == 0)
+		if (readers.size() == 0)
 			return false;
 		List<CamSession> filteredReader = filterReaders(req);
-		if(filteredReader.size() == 0)
+		if (filteredReader.size() == 0)
 			return false;
 		CamSession session = filteredReader.get(readerRoundRobin++ % filteredReader.size());
 		session.setLastRequest(req);
@@ -155,15 +188,15 @@ public class CardServer {
 	 */
 	private static List<CamSession> filterReaders(EcmRequest req) {
 		ArrayList<CamSession> filtered = new ArrayList<CamSession>();
-		for(CamSession session : readers){
-			//Remove readers that do not support that cardId.
-			if(!session.hasCard(req.getCardId())){
+		for (CamSession session : readers) {
+			// Remove readers that do not support that cardId.
+			if (!session.hasCard(req.getCardId())) {
 				continue;
 			}
-			for(int group : req.getGroups()){
-				logger.debug("filterReaders: attempting match: "+group+" "+listToStr(session.getGroups()));
-				if(session.getGroups().contains(group)){
-					logger.debug("filterReaders: found match: "+group);
+			for (int group : req.getGroups()) {
+				logger.debug("filterReaders: attempting match: " + group + " " + listToStr(session.getGroups()));
+				if (session.getGroups().contains(group)) {
+					logger.debug("filterReaders: found match: " + group);
 					filtered.add(session);
 				}
 			}
@@ -177,7 +210,7 @@ public class CardServer {
 	 */
 	private static String listToStr(ArrayList<Integer> groups) {
 		StringBuilder build = new StringBuilder("[");
-		for(int group : groups){
+		for (int group : groups) {
 			build.append(group);
 			build.append(", ");
 		}
@@ -208,8 +241,8 @@ public class CardServer {
 	 */
 	public static void handleClientEcmRequest(CamSession session, int cardId, int provider, int shareId, int serviceId, byte[] ecm) {
 		EcmRequest answer = handleEcmRequest(session, cardId, provider, 0, serviceId, ecm);
-		if(answer != null && answer.hasAnswer()){
-			logger.info("cache hit for "+session+" "+ChannelList.getChannelName(cardId, serviceId));
+		if (answer != null && answer.hasAnswer()) {
+			logger.info("cache hit for " + session + " " + ChannelList.getChannelName(cardId, serviceId));
 			session.getPacketSender().writeEcmAnswer(answer.getDcw());
 		}
 	}
