8c6dc3fe546a4632edb20d098c0ee0b643f275de
Martin <martin.chapman4@mail.dcu.ie>
2017-01-28 15:40:59 +0000

    Newcamd reconnects implemented

--- atum-jvcp/src/main/java/org/atum/jvcp/net/codec/newcamd/NewcamdClient.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/newcamd/NewcamdClient.java
@@ -2,6 +2,7 @@ package org.atum.jvcp.net.codec.newcamd;
 
 import org.atum.jvcp.NewcamdServer;
 import org.atum.jvcp.account.Account;
+import org.atum.jvcp.model.CamClient;
 import org.atum.jvcp.net.ClientConnector;
 import org.atum.jvcp.net.NetworkConstants;
 import org.atum.jvcp.net.codec.newcamd.io.NewcamdClientLoginDecoder;
@@ -14,25 +15,40 @@ import io.netty.channel.ChannelHandlerContext;
  * @since 3 Dec 2016 14:57:43
  */
 
-public class NewcamdClient extends NewcamdSession {
+public class NewcamdClient extends NewcamdSession implements CamClient {
 
-	public NewcamdClient(ChannelHandlerContext context,NewcamdServer server, byte[] desKey) {
+	private String host;
+	private int port;
+	private Account account;
+	NewcamdServer server;
+	
+	public NewcamdClient(String host, int port, Account account, ChannelHandlerContext context,NewcamdServer server, byte[] desKey) {
 		super(server, context, desKey);
+		this.host = host;
+		this.port = port;
+		this.account = account;
+		this.server = server;
 	}
 
 	public static NewcamdClient connect(NewcamdServer server, String host,int port, Account account, byte[] desKey){
-		ClientConnector conn = new ClientConnector();
-		
-		Channel channel = conn.connect(host, port, account, new NewcamdPipeline(server, NewcamdClientLoginDecoder.class));
-		NewcamdClient client = new NewcamdClient(channel.pipeline().firstContext(), server, desKey);
-		channel.attr(NetworkConstants.CAM_SESSION).set(client);
+		ChannelHandlerContext ctx = null;
+		NewcamdClient client = new NewcamdClient(host, port, account, ctx, server, desKey);
 		client.setReader(true);
-		server.registerSession(client);
+		client.connect();
 		return client;
 	}
 	
 	public String toString(){
 		return getAccount()+"@"+getCtx().channel().remoteAddress();
+	}
+
+	public void connect() {
+		Channel channel = ClientConnector.connect(host, port, account, new NewcamdPipeline(server, NewcamdClientLoginDecoder.class));
+		if(channel != null){
+			this.setCtx(channel.pipeline().firstContext());
+			channel.attr(NetworkConstants.CAM_SESSION).set(this);
+			server.registerSession(this);
+		}
 	}
 
 }
