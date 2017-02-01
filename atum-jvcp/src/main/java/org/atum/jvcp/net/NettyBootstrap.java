233d11939aa1c79f4116cf9331602536c07e31d1
atum-martin <martin.chapman4@mail.dcu.ie>
2017-02-01 22:53:59 +0000

    Netty UDP port listening code.

--- atum-jvcp/src/main/java/org/atum/jvcp/net/NettyBootstrap.java
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/NettyBootstrap.java
@@ -2,11 +2,13 @@ package org.atum.jvcp.net;
 
 import org.apache.log4j.Logger;
 
+import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioDatagramChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 /**
@@ -21,7 +23,7 @@ public class NettyBootstrap {
 	}
 	
 	public static void listenUdp(ChannelInitializer<SocketChannel> pipeline, int port) {
-		//listen(pipeline, port, NioDatagramChannel.class, new Bootstrap());
+		listenUdp(pipeline, port, new Bootstrap());
 	}
 	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -34,10 +36,20 @@ public class NettyBootstrap {
 
 		bootstrap.group(loopGroup).channel(clazz).childHandler(pipeline).bind(port).syncUninterruptibly();
 
-		logger.info("Server listening on port: " + port);
+		logger.info("TCP Server listening on port: " + port);
 		
-		//@SuppressWarnings("unused")
-		//CCcamClient client = CCcamClient.connect(cCcamServer, "127.0.0.1", 12000);
+	}
+	
+	public static void listenUdp(ChannelInitializer<SocketChannel> pipeline, int port,Bootstrap bootstrap) {
+		//port = 8080;
+ 
+		Logger logger = Logger.getLogger(NettyBootstrap.class);
+
+		EventLoopGroup loopGroup = new NioEventLoopGroup();	
+
+		bootstrap.group(loopGroup).channel(NioDatagramChannel.class).handler(pipeline).bind(port).syncUninterruptibly();
+
+		logger.info("UDP Server listening on port: " + port);
 		
 	}
 
