15129c1867023959664f0e3facfd85d44d4e6cdd
Martin <martin.chapman4@mail.dcu.ie>
2017-02-03 20:45:34 +0000

    Camd35Session

---
+++ atum-jvcp/src/main/java/org/atum/jvcp/net/codec/camd35/Camd35Session.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package org.atum.jvcp.net.codec.camd35;
+
+/**
+ * @author <a href="https://github.com/atum-martin">atum-martin</a>
+ * @since 3 Feb 2017
+ */
+public class Camd35Session {
+
+}
